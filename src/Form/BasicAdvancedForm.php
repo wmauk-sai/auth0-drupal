55fced16551313ca031239d0e33bc078ca9e9958
Jim LoVerde <loverde@nvisia.com>
2016-10-11 13:37:06 -0500

    added custom support for attributes and for mapping roles

--- src/Form/BasicAdvancedForm.php
+++ src/Form/BasicAdvancedForm.php
@@ -77,6 +77,52 @@ class BasicAdvancedForm extends FormBase {
         '#description' => t('This should be a valid JSON file. This entire object will be passed to the lock options parameter.')
     );
 
+    $form['auth0_auto_register'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Auto Register Auth0 users (ignore site registration settings)'),
+      '#default_value' => $config->get('auth0_auto_register', FALSE),
+      '#description' => t('Enable this option if you want new auth0 users to automatically be activated within Drupal regardless of the global site visitor registration settings (e.g. requiring admin approval).'),
+    );
+
+    // Enhancement to support mapping claims to user attributes and to roles
+    $form['auth0_claim_mapping'] = array(
+      '#type' => 'textarea',
+      '#title' => t('Mapping of Claims to Profile Fields (one per line):'),
+      '#cols' => 50,
+      '#rows' => 5,
+      '#default_value' => $config->get('auth0_claim_mapping'),
+      '#description' => t('Enter claim mappings here in the format &lt;claim_name>|&lt;profile_field_name> (one per line), e.g:
+<br/>given_name|field_first_name
+<br/>family_name|field_last_name
+<br/>
+<br/>NOTE: the following Drupal fields are handled automatically and will be ignored if specified above:
+<br/>    uid, name, mail, init, is_new, status, pass
+<br/>&nbsp;
+'),
+    );
+
+    $form['auth0_claim_to_use_for_role'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Claim for Role Mapping:'),
+      '#default_value' => $config->get('auth0_claim_to_use_for_role'),
+      '#description' => t('Name of the claim to use to map to Drupal roles, e.g. roles.  If the claim contains a list of values, all values will be used in the mappings below.')
+    );
+
+    $form['auth0_role_mapping'] = array(
+      '#type' => 'textarea',
+      '#title' => t('Mapping of Claim Role Values to Drupal Roles (one per line)'),
+      '#default_value' => $config->get('auth0_role_mapping'),
+      '#description' => t('Enter role mappings here in the format &lt;auth0 claim value>|&lt;drupal role name> (one per line), e.g.:
+<br/>admin|administrator
+<br/>poweruser|power users
+<br/>
+<br/>NOTE: for any drupal role in the mapping, if a user is not mapped to the role, the role will be removed from their profile.
+Drupal roles not listed above will not be changed by this module.
+<br/>&nbsp;
+')
+    );
+
+
     $form['actions']['#type'] = 'actions';
     $form['actions']['submit'] = array(
       '#type' => 'submit',
@@ -106,7 +152,11 @@ class BasicAdvancedForm extends FormBase {
             ->set('auth0_widget_cdn', $form_state->getValue('auth0_widget_cdn'))
             ->set('auth0_requires_verified_email', $form_state->getValue('auth0_requires_verified_email'))
             ->set('auth0_login_css', $form_state->getValue('auth0_login_css'))
+            ->set('auth0_auto_register', $form_state->getValue('auth0_auto_register'))
             ->set('auth0_lock_extra_settings', $form_state->getValue('auth0_lock_extra_settings'))
+            ->set('auth0_claim_mapping', $form_state->getValue('auth0_claim_mapping'))
+            ->set('auth0_claim_to_use_for_role', $form_state->getValue('auth0_claim_to_use_for_role'))
+            ->set('auth0_role_mapping', $form_state->getValue('auth0_role_mapping'))
             ->save();
   }
 
