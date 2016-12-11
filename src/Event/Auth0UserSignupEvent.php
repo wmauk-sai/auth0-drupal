70bdf27115148093d74bffc3fddd9296e1853c63
Eric Peterson <iamEAP@users.noreply.github.com>
2016-12-11 15:05:26 -0800

    Populate auth0Profile property in signup event.

--- src/Event/Auth0UserSignupEvent.php
+++ src/Event/Auth0UserSignupEvent.php
@@ -18,6 +18,7 @@ class Auth0UserSignupEvent extends Event {
    */
   public function __construct($user, $auth0Profile) {
     $this->user = $user;
+    $this->auth0Profile = $auth0Profile;
   }
 
   /**
