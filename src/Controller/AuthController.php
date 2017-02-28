eafa805c405dc5e55c078b3f4ad5998e5754dbb4
Rob Miroballi <rmiroballi@gmail.com>
2017-02-28 11:09:19 -0600

    Removed some extra logging

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -8,7 +8,7 @@ namespace Drupal\auth0\Controller;
 
 // Create a variable to store the path to this module and load vendor files if they exist
 define('AUTH0_PATH', drupal_get_path('module', 'auth0'));
-\Drupal::logger('auth0')->notice('AUTH0_PATH: '.AUTH0_PATH);
+
 if (file_exists(AUTH0_PATH . '/vendor/autoload.php')) {
   require_once (AUTH0_PATH . '/vendor/autoload.php');
 }
