aca13948f11e484839c413fd224a62c4271eedef
Rob Shepherd <rob@dhmedia.com.au>
2018-06-26 10:10:02 +0800

    Updated Auth0UserSigninEvent for Drupal Coding Standards

--- src/Event/Auth0UserSigninEvent.php
+++ src/Event/Auth0UserSigninEvent.php
@@ -1,43 +1,61 @@
 <?php
+
 namespace Drupal\auth0\Event;
 
 use Symfony\Component\EventDispatcher\Event;
+use Drupal\user\UserInterface;
 
 /**
  * User signin event.
  */
 class Auth0UserSigninEvent extends Event {
 
+  /**
+  * The event name.
+  */
   const NAME = 'auth0.signin';
 
   /**
+   * The current user.
+   *
    * @var \Drupal\user\UserInterface
    */
   protected $user;
 
   /**
+   * The Auth0 profile.
+   *
    * @var array
    */
   protected $auth0Profile;
-  
+
   /**
+   * The refresh token.
+   *
    * @var string
    */
   protected $refreshToken;
-  
+
   /**
+   * The timestamp when the token expires.
+   *
    * @var timestamp
    */
   protected $expiresAt;
-  
+
   /**
    * Initialize the event.
+   *
    * @param \Drupal\user\UserInterface $user
+   *   The current user.
    * @param array $auth0Profile
-   * @param string $refreshToken the refresh token
-   * @param timestamp $expiresAt the time when the idToken expires in unix timestamp (seconds only)
+   *   The Auth0 profile array.
+   * @param string $refreshToken
+   *   The refresh token.
+   * @param string $expiresAt
+   *   The time when the ID Token expires in unix timestamp (seconds only).
    */
-  public function __construct($user, $auth0Profile, $refreshToken, $expiresAt) {
+  public function __construct(UserInterface $user, array $auth0Profile, $refreshToken, $expiresAt) {
     $this->user = $user;
     $this->auth0Profile = $auth0Profile;
     $this->refreshToken = $refreshToken;
@@ -46,7 +64,9 @@ class Auth0UserSigninEvent extends Event {
 
   /**
    * Get the drupal user.
+   *
    * @return \Drupal\user\UserInterface
+   *   The current user.
    */
   public function getUser() {
     return $this->user;
@@ -54,7 +74,9 @@ class Auth0UserSigninEvent extends Event {
 
   /**
    * Get the Auth0 profile.
+   *
    * @return array
+   *   The Auth0 profile array.
    */
   public function getAuth0Profile() {
     return $this->auth0Profile;
@@ -62,7 +84,9 @@ class Auth0UserSigninEvent extends Event {
 
   /**
    * Get the refresh token.
-   * @return string token
+   *
+   * @return string
+   *   The refresh token.
    */
   public function getRefreshToken() {
     return $this->refreshToken;
@@ -70,11 +94,12 @@ class Auth0UserSigninEvent extends Event {
 
   /**
    * Get the time when the ID token expires.
-   * @return unix time when token expires
+   *
+   * @return timestamp
+   *   The unix time when token expires.
    */
   public function getExpiresAt() {
     return $this->expiresAt;
   }
-
 
 }
