b54147b1cc927e02972f437d178b37d275251358
Rob Shepherd <rob@dhmedia.com.au>
2018-06-26 20:27:29 +0800

    PHPCS for Auth0UserSignupEvent.php

--- src/Event/Auth0UserSignupEvent.php
+++ src/Event/Auth0UserSignupEvent.php
@@ -1,38 +1,52 @@
 <?php
+
 namespace Drupal\auth0\Event;
 
 use Symfony\Component\EventDispatcher\Event;
+use Drupal\user\UserInterface;
 
 /**
  * User signup event.
  */
 class Auth0UserSignupEvent extends Event {
 
+  /**
+   * The event name.
+   */
   const NAME = 'auth0.signup';
 
   /**
+   * The current user.
+   *
    * @var \Drupal\user\UserInterface
    */
   protected $user;
 
   /**
+   * The Auth0 profile.
+   *
    * @var array
    */
   protected $auth0Profile;
 
   /**
    * Initialize the event.
+   *
    * @param \Drupal\user\UserInterface $user
+   *   The current user.
    * @param array $auth0Profile
+   *   The Auth0 profile array.
    */
-  public function __construct($user, $auth0Profile) {
+  public function __construct(UserInterface $user, array $auth0Profile) {
     $this->user = $user;
     $this->auth0Profile = $auth0Profile;
   }
 
   /**
    * Get the drupal user.
+   *
    * @return \Drupal\user\UserInterface
+   *   The current user.
    */
   public function getUser() {
     return $this->user;
@@ -40,7 +54,9 @@ class Auth0UserSignupEvent extends Event {
 
   /**
    * Get the Auth0 profile.
+   *
    * @return array
+   *   The Auth0 profile.
    */
   public function getAuth0Profile() {
     return $this->auth0Profile;
